0d4a269ec433277247cb5a18e4323600f33f878c
Andreas Gomes Marchi <162516125+AndreasGomesMarchi@users.noreply.github.com>
2024-09-10 18:33:01 -0300

    Update Q3.cpp
    Adição da biblioteca <stdlib.h> e da função system("pause")

--- N2/Q3.cpp
+++ N2/Q3.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
     float n1, n2;
@@ -17,6 +18,7 @@ int main(){
     }else {
         printf("Os dois numeros sao iguais!");
     }
-    
+
+    system("pause");
     return 0;
 }
